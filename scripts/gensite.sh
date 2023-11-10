d16b92bc2b9d61386bbff9055ae03723223a2891
Nicholas Di Girolamo <njd131@scarletmail.rutgers.edu>
2023-11-10 14:33:35 -0500

    Allow the script to use both absolute and relative paths.

--- scripts/gensite.sh
+++ scripts/gensite.sh
@@ -36,17 +36,24 @@ BUILD_DIR="../slug"
 SOURCE_DIR="../drafts"
 
 while getopts b:d:h opt; do
-    case $opt in
-	b) BUILD_DIR="$OPTARG";;
+    case "$opt" in
+	b) BUILD_DIR="$OPTARG";; 
 	d) SOURCE_DIR="$OPTARG";;
 	h) usage;;
-      esac
+    esac
 done
 
 if [ ! -d "$SOURCE_DIR" ]; then
     printf "Source directory not found!\n"
     exit 1
 fi
+
+# Check if the build directory is absolute. If it is not, 
+# then make it absolute. 
+case "$BUILD_DIR" in
+    /*) ;;
+    *) BUILD_DIR="$(pwd)/$BUILD_DIR"; echo "$BUILD_DIR";;
+esac	
 
 if [ ! -e "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
